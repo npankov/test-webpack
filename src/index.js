f6c3e61af16f351b5a0fde3b0a2aea5ff79b8fb5
n.pankov <pankoff2000@gmail.com>
2020-08-20 14:23:30 +0200

    add json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Post from "./Post";
 import './styles/styles.css';
+import json from './assets/json.json';
 
 const post = new Post('webpack Post title');
 console.log('Post to String:', post.toString());
+console.log('JSON: ', json);
