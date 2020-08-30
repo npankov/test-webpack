b52930dc625040fc5b48450a754aa555735bd26d
n.pankov <pankoff2000@gmail.com>
2020-08-30 17:26:22 +0200

    css normalize pack, add xml, csv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import Post from "./Post";
 import './styles/styles.css';
 import json from './assets/json.json';
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
 import logo from './assets/webpack-logo.png';
 
 const post = new Post('Webpack Post title', logo);
 console.log('Post to String:', post.toString());
-console.log('JSON: ', json);
+console.log('JSON:', json);
+console.log('XML:', xml);
+console.log('CSV:', csv);
