af1801a16924e19d26a417d580c3b529299d5c66
n.pankov <pankoff2000@gmail.com>
2020-09-01 16:45:17 +0200

    devMode hmr

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,81 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV === 'development';
+console.log('devMode:', devMode);
 
 module.exports = {
     context: path.resolve(__dirname,'src'),
     mode: 'development',
     entry: {
         main: './index.js',
-        analytics: './analytics.js'
+        analytics: './analytics.js',
     },
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
         alias: {
-            '@normalize': path.resolve(__dirname, 'node_modules/normalize.css')
+            '@normalize': path.resolve(__dirname, 'node_modules/normalize.css'),
         }
     },
     optimization: {
         splitChunks: {
-            chunks: 'all'
+            chunks: 'all',
         }
     },
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: devMode,
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
         }),
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
+                    to: path.resolve(__dirname, 'dist'),
                 }
             ]
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
+            filename: '[name].[contenthash].css',
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader',]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: devMode,
+                            reloadAll: true,
+                        },
+                    },
+                    'css-loader',
+                ],
             },
             {
                 test: /\.(png|svg|jpe?g|gif)$/,
-                use: ['file-loader']
+                use: ['file-loader'],
             },
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
+                use: ['file-loader'],
             },
             {
                 test: /\.xml$/,
-                use: ['xml-loader']
+                use: ['xml-loader'],
             },
             {
                 test: /\.csv$/,
-                use: ['csv-loader']
+                use: ['csv-loader'],
             }
         ],
     },
