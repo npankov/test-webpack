48876c471543552b0f6a4b282d44878832be533b
n.pankov <pankoff2000@gmail.com>
2020-09-08 14:18:35 +0200

    add babel/preset/polyfill/plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as $ from 'jquery';
 import Post from './models/Post';
+import './babel';
 import './styles/styles.css';
 import './styles/less.less';
 import './styles/scss.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     context: path.resolve(__dirname,'src'),
     mode: 'development',
     entry: {
-        main: './index.js',
+        main: ['@babel/polyfill', './index.js'],
         analytics: './analytics.js',
     },
     output: {
@@ -117,7 +117,19 @@ module.exports = {
             {
                 test: /\.csv$/,
                 use: ['csv-loader'],
-            }
+            },
+            {   test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                    ],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                    ]
+                }
+            },
         ],
     },
 };
